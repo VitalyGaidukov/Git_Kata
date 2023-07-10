9a6c6aa285723a6465a6959cffbe25d24cef2bd5
Vitaly Gaidukov <vitaly.gaidukov@gmail.com>
2023-07-10 15:21:09 +0300

    Add new class Person

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Person {
+    private int Age;
+    private double Salary;
+    private String Name;
+
+    public Person(int age, double salary, String name) {
+        Age = age;
+        Salary = salary;
+        Name = name;
+    }
+}
